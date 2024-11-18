49fae1924275302456d3b0788045880776a41ca9
Dylan Barduzzi <dlbarduzzi@gmail.com>
2024-11-18 15:45:29 -0600

    Add initial http server

--- cmd/guestbook/main.go
+++ cmd/guestbook/main.go
@@ -1,10 +1,30 @@
 package main
 
 import (
+	"context"
+	"os"
+
 	"github.com/dlbarduzzi/guestbook/internal/logging"
+	"github.com/dlbarduzzi/guestbook/internal/server"
 )
 
 func main() {
-	logger := logging.NewLoggerFromEnv()
-	logger.Info("Hello")
+	logger := logging.NewLoggerFromEnv().With("app", "guestbook")
+
+	ctx := context.Background()
+	ctx = logging.LoggerWithContext(ctx, logger)
+
+	if err := start(ctx); err != nil {
+		logger.Error(err.Error())
+		os.Exit(2)
+	}
+}
+
+func start(ctx context.Context) error {
+	logger := logging.LoggerFromContext(ctx)
+	logger.Info("app running...")
+
+	srv := server.NewServer(8000)
+
+	return srv.Start(ctx, nil)
 }
